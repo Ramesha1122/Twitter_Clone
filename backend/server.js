5c4c9f2bcf5b22d892e6c7eb060721463b303647
Ramesha1122 <rameshanamasingha8@gmail.com>
2025-03-18 06:45:18 +0530

    1.notification routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@ import {v2 as cloudinary} from "cloudinary"
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
 import postRoutes from './routes/post.routes.js';
-
+import notificationRoutes from './routes/notification.routes.js';
+ 
 
 // Load env vars    
 dotenv.config();
@@ -29,6 +30,7 @@ app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
+app.use('/api/notifications', notificationRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
